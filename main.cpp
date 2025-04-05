5f4fa552aefa2d09aa0030c19adde8f126b60b30
Iskander-rzk <sasha1koval@gmail.com>
2025-04-05 12:31:33 +0700

    fd

--- main.cpp
+++ main.cpp
@@ -5,21 +5,26 @@
 class Weapon {
 protected:
     std::string Brand;
+    int Cost;
 public:
-    Weapon(const std::string& brand) : Brand(brand) {}
+    Weapon(const std::string& brand, int cost) : Brand(brand, cost) {}
     virtual void ShowInfo() const {
-        std::cout << "Brand: " << Brand << std::endl; 
+        std::cout << "Brand: " << Brand << " " <<  Cost << "$" << std::endl ; 
+    }
+    
+    void HelloWorld()  {
+        std::cout << "Hello World!";
     }
     virtual ~Weapon() {}
 };
 
 class Firearm : public Weapon {
 protected:
-    std::string Category; // 0 - automatic, 1 - semi-auto, 2 - burst, 3 - etc.
+    std::string Category; 
 public:
-    Firearm(const std::string& brand, const std::string& category) : Weapon(brand), Category(category) {}
+    Firearm(const std::string& brand, int cost, const std::string& category) : Weapon(brand, cost), Category(category) {}
     void ShowInfo() const override {
-        std::cout << "Weapon Type: " << Brand << ", Category: " << Category << std::endl; 
+        std::cout << "Weapon Type: " << Brand << ", Category: " << Category << " " <<  Cost << "$" << std::endl; 
     }
 };
 
@@ -27,11 +32,14 @@ class AssaultRifle : public Firearm {
 private:
     int Caliber;
 public:
-    AssaultRifle(const std::string& brand, const std::string& category, int caliber) 
-        : Firearm(brand, category), Caliber(caliber) {}
+    AssaultRifle(const std::string& brand, int cost, const std::string& category, int caliber) 
+        : Firearm(brand, cost, category), Caliber(caliber) {}
     void ShowInfo() const override {
         std::cout << "Assault Rifle Brand: " << Brand << ", Category: " << Category 
-                  << ", Caliber: " << Caliber << " mm" << std::endl;
+                  << ", Caliber: " << Caliber << " mm" << " " <<  Cost << "$" << std::endl;
+    }
+    void HelloWorld()  {
+        std::cout << "Hello World!";
     }
 };
 
@@ -40,11 +48,11 @@ private:
     int BeltCapacity;
     int Caliber;
 public:
-    MachineGun(const std::string& brand, const std::string& category, int caliber, int capacity) 
-        : Firearm(brand, category), Caliber(caliber), BeltCapacity(capacity) {}
+    MachineGun(const std::string& brand, int cost, const std::string& category, int caliber, int capacity) 
+        : Firearm(brand, cost, category), Caliber(caliber), BeltCapacity(capacity) {}
     void ShowInfo() const override {
         std::cout << "Machine Gun Brand: " << Brand << ", Category: " << Category 
-                  << ", Caliber: " << Caliber << " mm, Belt Capacity: " 
+                  << ", Caliber: " << Caliber << " mm, Belt Capacity: " " " <<  Cost << "$" 
                   << BeltCapacity << " rounds" << std::endl;
     }
 };
@@ -63,24 +71,25 @@ private:
     int MaxDistance;
     int Caliber;
 public:
-    SniperRifle(const std::string& brand, const std::string& category, int caliber, int maxDistance, Sniper* sniper) 
-        : Firearm(brand, category), Caliber(caliber), MaxDistance(maxDistance), _Sniper(sniper) {}
+    SniperRifle(const std::string& brand, int cost, const std::string& category, int caliber, int maxDistance, Sniper* sniper) 
+        : Firearm(brand, cost, category), Caliber(caliber), MaxDistance(maxDistance), _Sniper(sniper) {}
     void ShowInfo() const override {
-        std::cout << "Sniper Rifle Brand: " << Brand << ", Category: " << Category 
+        std::cout << "Sniper Rifle Brand: " << Brand << ", Category: " << " " <<  Cost << "$" << Category 
                   << ", Caliber: " << Caliber << " mm, Max Distance: " << MaxDistance 
                   << " meters, Sniper: " << _Sniper->GetName() << std::endl;
     }
 };
 
 int main() {
-    Weapon car("Toyota");
-    AssaultRifle scar("SCAR", "automatic", 556);
+    Weapon cold("Katana", 500);
+    AssaultRifle scar("SCAR", 2500, "automatic", 556);
     Sniper john("John Doe");
-    SniperRifle barret("Barrett", "bolt-action", 50, 1500, &john);
-
-    car.ShowInfo();
+    SniperRifle barret("Barrett", 4000, "bolt-action", 50, 1500, &john);
+    Weapon* rifle = &scar;
+    cold.ShowInfo();
     scar.ShowInfo();
     barret.ShowInfo();
-    
+    rifle->ShowInfo();
+    rifle->HelloWorld();
     return 0;
 }
